bfea8ea7a9574e9f43f0376e73ed0dbe431920b6
trikiedick <106841038+trikiedick@users.noreply.github.com>
2022-06-03 13:21:20 -0700

    Add files via upload

---
+++ spoty-records-master/config.py
@@ -0,0 +1,13 @@
+"""
+Created on Wed Jan 29 10:29:55 2020
+
+@author: vlad
+"""
+
+#insert local parameters
+
+username = 'trikiedick'
+client_id ='189a515b62284215a5b7257e43b3dd4b'
+client_secret = 'e83bb00e2c9048b3805a71c284d061ce'
+redirect_uri = 'http://localhost:7777/callback'
+scope = 'user-read-recently-played'
